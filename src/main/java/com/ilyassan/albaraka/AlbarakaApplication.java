48b2f0546aecb22d9c6b01a6e522ee0294d4de69
ilyass anida <anidailyass@gmail.com>
2025-12-17 15:59:27 +0100

    first commit

---
+++ src/main/java/com/ilyassan/albaraka/AlbarakaApplication.java
@@ -0,0 +1,13 @@
+package com.ilyassan.albaraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbarakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbarakaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilyassan/albaraka/AlbarakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyassan.albaraka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbarakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
